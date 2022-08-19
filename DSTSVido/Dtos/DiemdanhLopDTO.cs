bfd7fef4f41d124216ed21fb05ffe5b8429f9035
sincollmm <81744519+sincollmm@users.noreply.github.com>
2022-08-19 21:32:32 +0700

    backup

--- DSTSVido/Dtos/DiemdanhLopDTO.cs
+++ DSTSVido/Dtos/DiemdanhLopDTO.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace DSTSVido.Dtos
 {
     public class DiemdanhLopDTO
-    {
+    { //backup
         public int? id { get; set; }
         public string ma { get; set; }
     }
